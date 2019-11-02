856b33e26484c4ee50c1c6ab57aed7ac7e69aa88
Ar-Ridho Gusti <ridhogusti0@gmail.com>
2019-11-02 22:08:58 +0700

    test 2

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import TextField from "./components/atoms/TextField";
 
 function App() {
   const [result1, setResult1] = useState({ __html: "" });
+  const [result2, setResult2] = useState({ __html: "" });
 
   const test1 = value => {
     let tempResult = "";
@@ -24,9 +25,36 @@ function App() {
     setResult1(tempResult);
   };
 
+  const test2 = value => {
+    let tempResult = "";
+    let tempStar = "";
+    for (let i = 0; i < value; i++) {
+      if (i === 0 || i === value - 1) {
+        for (let j = 0; j < value; j++) {
+          tempStar += "*";
+        }
+        tempStar = `${tempStar}<br />`;
+      }
+      if (i > 0 && i < value - 1) {
+        for (let j = 0; j < value; j++) {
+          if (j === 0 || j === value - 1) {
+            tempStar += "*";
+          } else if (j >= 1 && j <= value - 1) {
+            tempStar += "&nbsp";
+          }
+        }
+        tempStar = `${tempStar}<br />`;
+      }
+    }
+    tempResult = { __html: tempStar };
+    setResult2(tempResult);
+  };
+
   const onChange = useCallback((id, fill) => {
     if (id === "test1") {
       test1(fill);
+    } else if (id === "test2") {
+      test2(fill);
     }
   }, []);
 
@@ -47,8 +75,18 @@ function App() {
             dangerouslySetInnerHTML={result1}
           ></div>
         </div>
-        <div label="Croc">
-          After &apos;while, <em>Crocodile</em>!
+        <div label="Test 2">
+          <TextField
+            onChange={e => onChange("test2", e.target.value)}
+            placeholder="Input your number"
+            type="number"
+          ></TextField>
+          <div
+            style={{
+              fontSize: "24px"
+            }}
+            dangerouslySetInnerHTML={result2}
+          ></div>
         </div>
         <div label="Sarcosuchus">
           Nothing to see here, this tab is <em>extinct</em>!
